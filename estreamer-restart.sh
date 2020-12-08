01e5a9df9e7cabb23a27016bfb19a36f78a969e1
mk-azure <68885798+mk-azure@users.noreply.github.com>
2020-12-08 19:07:55 +0530

    Create estreamer-restart.sh

---
+++ estreamer-restart.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+#
+# Script for estreamer workaround 
+#
+LOGFILE=/apps/splunk-forwarder/splunk/etc/apps/TA-eStreamer/bin/encore/estreamer.log 
+PIDFILE=/apps/splunk-forwarder/splunk/etc/apps/TA-eStreamer/data/10.20.178.27-8302_proc.pid 
+PS_STRING=estreamer 
+SPLUNK_CMD=/apps/splunk-forwarder/splunk/bin/splunk 
+
+LAST_ERR_TS=""
+LATEST_ERR_TS="$(grep 'ERROR.*PID file already exists' ${LOGFILE} | tail -1 | awk '{print $1,$2}')"
+
+if [ "$LATEST_ERR_TS" == "$LAST_ERR_TS" ]
+then 
+  echo "No error noticed" 
+else 
+  LAST_ERR_TS="$LAST_ERR_TS"
+  rm -f ${PIDFILE}
+  ps -ef | grep -i ${PS_STRING} | grep -v grep | xargs kill 
+  ps -ef | grep -i ${PS_STRING} | grep -v grep | while read pid; do kill -9 ${pid} > /dev/null 2>&1; done 
+  ${SPLUNK_CMD} restart 
+fi 
